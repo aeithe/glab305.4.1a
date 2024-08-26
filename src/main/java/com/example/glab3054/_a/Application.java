c9754e5b9666af80a9a4988ce7f097386226c59d
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-08-26 08:27:48 -0400

    first commit

---
+++ src/main/java/com/example/glab3054/_a/Application.java
@@ -0,0 +1,13 @@
+package com.example.glab3054._a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
